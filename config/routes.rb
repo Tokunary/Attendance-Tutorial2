6f456ac1e578f1a64597f0a5d942ebd3fc394fff
Tokunary <t0kufa_y@yahoo.co.jp>
2020-01-07 16:32:45 +0000

    タスク一覧と新規ページタイトルのみ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
-  get 'task/index'
-
+  
   root 'static_pages#top'
   get '/signup', to: 'users#new'
   
   get    '/login', to: 'sessions#new'
   post   '/login', to: 'sessions#create'
+  
+  get 'tasks/new', to: 'tasks#new'
+  get 'tasks/index', to: 'tasks#index'
+  get 'tasks/:id', to: 'tasks#show'
+
+ 
   delete '/logout', to: 'sessions#destroy'
   
   resources :users
