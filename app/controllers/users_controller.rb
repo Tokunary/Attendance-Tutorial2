e06233a9a20aae043a7df02e32252421bd157a2f
Tokunary <t0kufa_y@yahoo.co.jp>
2020-01-30 14:55:26 +0000

    ログイン済みのユーザーか確認

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,10 +22,28 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+  
+  def update
+    @user= User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] ="ユーザー情報を更新しました。"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
  
   private
   
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
+    
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] ="ログインしてください。"
+        rendirect_to login_url
+      end
+    end
+      
 end
