d975cfd1ff026ac49611a5f8bd95b66d8be12cad
Tokunary <t0kufa_y@yahoo.co.jp>
2020-02-05 15:32:27 +0000

    ユーザー一覧　end

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,12 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update]
+  before_action :logged_in_user, only: [:index, :show, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
-  before_action :correct_user, only: [:show, :edit, :update]
   
+  def index
+    @users = User.all
+  end
+    
   def show
   end
 
@@ -39,16 +43,20 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
     
+    def set_user
+      @user = User.find(params[:id])
+    end
+    
     def logged_in_user
       unless logged_in?
+        store_location
         flash[:danger] ="ログインしてください。"
-        rendirect_to login_url
+        redirect_to login_url
       end
     end
     
     def correct_user
-      @user = User.find(params[:id])
-      redirect_to(root_url) unless @user == current_user
+      redirect_to(root_url) unless current_user?(@user)
     end
       
 end
